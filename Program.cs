0dd2c9d82a8e56ae7b04dda39f6e44ff0d68d640
Saeed Davari <sa.davari84@gmail.com>
2016-01-24 12:35:33 +0100

    Adding Files

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenResolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+           List<Resolution> availableResolutions = Resolution.GetAvailableScreenResolutions();
+
+           int count = 1;
+           foreach (var resolution in availableResolutions)
+           {
+               Console.WriteLine("{0}. Width: {1}, Height: {2}",count, resolution.Width,resolution.Height);
+               count++;
+           }
+
+           Console.WriteLine("");
+            Console.WriteLine("Select the Screen Resolution (1-{0})", count-1);
+            Console.WriteLine("");
+
+            int answer = Convert.ToInt32(Console.ReadLine());
+
+            ScreenResolution.Resolution.SetScreenResolution(availableResolutions.ElementAt(answer-1).Width, availableResolutions.ElementAt(answer-1).Height);
+            
+
+        }
+    }
+}
---
+++ Resolution.cs
@@ -0,0 +1,178 @@
+
+using System;
+//using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Linq;
+
+
+[StructLayout(LayoutKind.Sequential)]
+public struct DEVMODE 
+{
+	[MarshalAs(UnmanagedType.ByValTStr,SizeConst=32)] public string dmDeviceName;
+	public short  dmSpecVersion;
+	public short  dmDriverVersion;
+	public short  dmSize;
+	public short  dmDriverExtra;
+	public int    dmFields;
+
+	public short dmOrientation;
+	public short dmPaperSize;
+	public short dmPaperLength;
+	public short dmPaperWidth;
+
+	public short dmScale;
+	public short dmCopies;
+	public short dmDefaultSource;
+	public short dmPrintQuality;
+	public short dmColor;
+	public short dmDuplex;
+	public short dmYResolution;
+	public short dmTTOption;
+	public short dmCollate;
+	[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)] public string dmFormName;
+	public short dmLogPixels;
+	public short dmBitsPerPel;
+	public int   dmPelsWidth;
+	public int   dmPelsHeight;
+
+	public int   dmDisplayFlags;
+	public int   dmDisplayFrequency;
+
+	public int   dmICMMethod;
+	public int   dmICMIntent;
+	public int   dmMediaType;
+	public int   dmDitherType;
+	public int   dmReserved1;
+	public int   dmReserved2;
+
+	public int   dmPanningWidth;
+	public int   dmPanningHeight;
+};
+
+
+
+class User_32
+{
+	[DllImport("user32.dll")]
+	public static extern int EnumDisplaySettings (string deviceName, int modeNum, ref DEVMODE devMode );         
+	[DllImport("user32.dll")]
+	public static extern int ChangeDisplaySettings(ref DEVMODE devMode, int flags);
+
+	public const int ENUM_CURRENT_SETTINGS = -1;
+	public const int CDS_UPDATEREGISTRY = 0x01;
+	public const int CDS_TEST = 0x02;
+	public const int DISP_CHANGE_SUCCESSFUL = 0;
+	public const int DISP_CHANGE_RESTART = 1;
+	public const int DISP_CHANGE_FAILED = -1;
+}
+
+
+namespace ScreenResolution
+{
+
+    
+
+	class Resolution
+	{
+
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+
+        public Resolution(int width, int height)
+		{
+
+
+            Width = width;
+            Height = height;
+                    
+		}
+
+        public static List<Resolution> GetAvailableScreenResolutions()
+        {
+            List<Resolution> availableResolutions = new List<Resolution>();
+
+            DEVMODE vDevMode = new DEVMODE();
+            int i = 0;
+            while (0 != User_32.EnumDisplaySettings(null, i, ref vDevMode))
+            {
+                if (vDevMode.dmDisplayFrequency == 60) 
+                {
+                //Console.WriteLine("Width:{0} Height:{1} Color:{2} Frequency:{3}",
+                //                        vDevMode.dmPelsWidth,
+                //                        vDevMode.dmPelsHeight,
+                //                        1 << vDevMode.dmBitsPerPel, vDevMode.dmDisplayFrequency
+                //                    );
+
+                Resolution res = new Resolution(vDevMode.dmPelsWidth, vDevMode.dmPelsHeight);
+
+                if (!availableResolutions.Any(r => r.Width == res.Width)) 
+                availableResolutions.Add(res);
+
+                }
+                
+                i++;
+            }
+
+            
+
+            return availableResolutions;
+
+        }
+
+        public static void SetScreenResolution(int width, int height)
+        {
+
+            int iWidth = width;
+            int iHeight = height;
+
+
+            DEVMODE dm = new DEVMODE();
+            dm.dmDeviceName = new String(new char[32]);
+            dm.dmFormName = new String(new char[32]);
+            dm.dmSize = (short)Marshal.SizeOf(dm);
+
+            if (0 != User_32.EnumDisplaySettings(null, User_32.ENUM_CURRENT_SETTINGS, ref dm))
+            {
+
+                dm.dmPelsWidth = iWidth;
+                dm.dmPelsHeight = iHeight;
+
+                int iRet = User_32.ChangeDisplaySettings(ref dm, User_32.CDS_TEST);
+
+                if (iRet == User_32.DISP_CHANGE_FAILED)
+                {
+                    throw new System.Exception("Unable to process your request");                   
+                }
+                else
+                {
+                    iRet = User_32.ChangeDisplaySettings(ref dm, User_32.CDS_UPDATEREGISTRY);
+
+                    switch (iRet)
+                    {
+                        case User_32.DISP_CHANGE_SUCCESSFUL:
+                            {
+                                break;
+
+                                //successfull change
+                            }
+                        case User_32.DISP_CHANGE_RESTART:
+                            {
+                                throw new System.Exception("Description: You Need To Reboot For The Change To Happen.\n If You Feel Any Problem After Rebooting Your Machine\nThen Try To Change Resolution In Safe Mode.");
+                                //windows 9x series you have to restart
+                            }
+                        default:
+                            {
+                                throw new System.Exception("Failed To Change The Resolution");
+                               
+                                //failed to change
+                            }
+                    }
+                }
+
+            }
+        }
+
+    }
+}
