1dc20a9dd283aa48ae6088cee300bfc2933633d4
Saeed Davari <sa.davari84@gmail.com>
2016-01-27 22:22:37 +0100

    Made class sortable and removed unnecessary comments
    Resolution class implements ICoimparable to be able to sort Lists of the
    class

--- Program.cs
+++ Program.cs
@@ -10,25 +10,36 @@ namespace ScreenResolution
     {
         static void Main(string[] args)
         {
+            try
+            {
+                List<Resolution> availableResolutions = Resolution.GetAvailableScreenResolutions();
+                availableResolutions.Sort();
+
+                int count = 1;
+                foreach (var resolution in availableResolutions)
+                {
+                    Console.WriteLine("{0}. Width: {1}, Height: {2}", count, resolution.Width, resolution.Height);
+                    count++;
+                }
+
+                Console.WriteLine("");
+                Console.WriteLine("Select the Screen Resolution (1-{0})", count - 1);
+                Console.WriteLine("");
+
+                int answer = Convert.ToInt32(Console.ReadLine());
+
+                ScreenResolution.Resolution.SetScreenResolution(availableResolutions.ElementAt(answer - 1).Width, availableResolutions.ElementAt(answer - 1).Height);           
+
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Error: " + ex.Message);
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+            }
            
-           List<Resolution> availableResolutions = Resolution.GetAvailableScreenResolutions();
-
-           int count = 1;
-           foreach (var resolution in availableResolutions)
-           {
-               Console.WriteLine("{0}. Width: {1}, Height: {2}",count, resolution.Width,resolution.Height);
-               count++;
-           }
-
-           Console.WriteLine("");
-            Console.WriteLine("Select the Screen Resolution (1-{0})", count-1);
-            Console.WriteLine("");
-
-            int answer = Convert.ToInt32(Console.ReadLine());
-
-            ScreenResolution.Resolution.SetScreenResolution(availableResolutions.ElementAt(answer-1).Width, availableResolutions.ElementAt(answer-1).Height);
-            
-
         }
     }
 }
